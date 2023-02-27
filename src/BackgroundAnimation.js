74ec5dd50216b2a799442ab29b26b6318fe6ce11
safarilewis <lleewwii1@gmail.com>
2023-02-27 17:16:19 +0300

    First commit

---
+++ src/BackgroundAnimation.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+export const BackgroundAnimation = () => {
+    return (
+        <ul className="background">
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+        </ul>
+    )
+}
---
+++ src/InputContainer.js
@@ -0,0 +1,27 @@
+import { useState } from "react";
+
+const InputContainer = ({ setInputValue }) => {
+    const [value, setValue] = useState("");
+
+    const handleClick = () => {
+        setInputValue(value);
+        setValue("");
+    }
+    return (
+        <div className='inputContainer'>
+            <h1>Shortn</h1>
+            <section className="inputText"><h2>Free URL shortener</h2>
+                <div className="inputBox">
+                    <input
+                        type="text"
+                        placeholder="Enter link here"
+                        value={value}
+                        onChange={e => setValue(e.target.value)}
+                    />
+                    <button onClick={handleClick}>Shorten</button>
+                </div>
+            </section>
+        </div>
+    )
+}
+export default InputContainer;
